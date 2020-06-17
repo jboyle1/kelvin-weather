13cfc42800c91368abf44db90ff96f508c5a22d2
Jed Boyle <jedboyle01@gmail.com>
2020-06-17 10:54:14 +0100

    Use the .floor() method from the built-in Math object to round down the Fahrenheit temperature. Save the result to the fahrenheit variable.

--- static/js/kelvin-weather.js
+++ static/js/kelvin-weather.js
@@ -7,5 +7,9 @@ const celsius = kelvin - 273;
 // Use this equation to calculate Fahrenheit, then store the answer in a variable named fahrenheit:
 
 // Fahrenheit = Celsius * (9/5) + 32
-
 let fahrenheit = celsius * (9 / 5) + 32;
+
+// Use the .floor() method from the built-in Math object to round down the Fahrenheit temperature. Save the result to the fahrenheit variable.
+fahrenheit = Math.floor(fahrenheit);
+
+
