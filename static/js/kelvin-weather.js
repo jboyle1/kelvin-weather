7a5902c787de0a17641dfec37b023e0a91aebf18
Jed Boyle <jedboyle01@gmail.com>
2020-06-17 10:52:30 +0100

    Use this equation to calculate Fahrenheit, then store the answer in a variable named fahrenheit: Fahrenheit = Celsius * (9/5) + 32.

--- static/js/kelvin-weather.js
+++ static/js/kelvin-weather.js
@@ -1,4 +1,11 @@
 // Create a variable named kelvin, and set it equal to 293.
 const kelvin = 293;
+
 // Celsius is similar to Kelvin — the only difference is that Celsius is 273 degrees less than Kelvin. Convert Kelvin to Celsius by subtracting 273 from the kelvin variable. Store the result in another variable, named celsius.
 const celsius = kelvin - 273;
+
+// Use this equation to calculate Fahrenheit, then store the answer in a variable named fahrenheit:
+
+// Fahrenheit = Celsius * (9/5) + 32
+
+let fahrenheit = celsius * (9 / 5) + 32;
